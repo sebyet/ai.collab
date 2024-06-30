d3fb5cf5889e94cd4be9591c5ee7b429122c5097
sebyet <sebastien.payet9@gmail.com>
2024-06-30 23:09:20 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -21,20 +21,22 @@ export default function Home() {
           alt="thumbnail"
         />
         <div className="flex flex-col gap-5 justify-center">
-        <Typography variant="heading1">
-          Unlock a better version of yourself.
-        </Typography>
-        <Typography className="mx-auto" variant="large">
-          We create custom learning journeys to help managers integrate AI tools & prompting techniques effectively in their daily work.
-        </Typography>
-        <div className="flex flex-row gap-3 flex-wrap">
-          <span className="border-brand bg-background border rounded-full text-md text-brand p-3">Boost your productivity</span>
-          <span className="border-brand bg-background border rounded-full text-brand p-3">Explore new business opportunities</span>
-          <span className="border-brand bg-background border rounded-full text-brand p-3">Superchage your work</span>
-          <span className="border-brand bg-background border rounded-full text-brand p-3">Get the best from yourself</span>
-        </div>
-        <Input onClearClick={() => {}} startIcon={<Mail />} type='text'/>
-        <Button className="w-fit" variant="default">Get more informations</Button>
+          <Typography variant="heading1">
+            Unlock a better version of yourself.
+          </Typography>
+          <Typography className="mx-auto" variant="large">
+            We create custom learning journeys to help managers integrate AI tools & prompting techniques effectively in their daily work.
+          </Typography>
+          <div className="flex flex-row gap-3 flex-wrap">
+            <span className="border-brand bg-background border rounded-full text-md text-brand p-3">Boost your productivity</span>
+            <span className="border-brand bg-background border rounded-full text-brand p-3">Explore new business opportunities</span>
+            <span className="border-brand bg-background border rounded-full text-brand p-3">Superchage your work</span>
+            <span className="border-brand bg-background border rounded-full text-brand p-3">Get the best from yourself</span>
+          </div>
+        
+          <Input placeholder="sebastien.payet@gmail.com" onClearClick={() => {}} startIcon={<Mail />} type='text'/>
+    
+          <Button className="w-fit" variant="default">Get more informations</Button>
       </div>
 
 
