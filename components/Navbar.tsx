05fdcfe3e2588c00de4245dde7a49b2120929dc4
sebyet <sebastien.payet9@gmail.com>
2024-07-15 19:39:13 +0200

    update logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar: React.FC = () => {
                 <ul className="flex justify-center items-center">
                     <li className='cursor pointer'>
                         <a className='flex flex-col hover:bg-muted px-3 py-2 rounded-lg items-center transition active:scale-[.95]' href='/'>
-                            <span className='text-2xl text-onBackground font-bold'>Apprendrai</span>
+                            <span className='text-2xl text-onBackground font-bold'>apprendrai</span>
                             <span className='text-sm text-onBackground font-normal'>AI Courses to Superchage your Skills</span>
                         </a>
                     </li>
