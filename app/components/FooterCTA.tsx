95b6b98447b0967aaf4fb2c67a34a454cc7dc3e4
sebyet <sebastien.payet9@gmail.com>
2024-07-30 17:40:30 +0200

    update

--- app/components/FooterCTA.tsx
+++ app/components/FooterCTA.tsx
@@ -8,10 +8,6 @@ import { usePathname } from "next/navigation";
 export function FooterCTA() {
   const pathname = usePathname();
 
-  if (pathname.includes("pitch")) {
-    return null;
-  }
-
   return (
     <div className="max-w-6xl gap-10 mx-auto border border-border rounded-xl text-center py-14 md:px-24 md:py-20 mb-32 mt-24 flex items-center flex-col bg-brand text-onBrand">
       <Typography variant="heading3" className="text-6xl lg:text-8xl font-medium">
@@ -19,13 +15,13 @@ export function FooterCTA() {
         <br />
         A Better Yourself.
       </Typography>
-      <div className="flex items-center space-x-5">
+      {!pathname.includes("talk-to-us") &&
         <Link href="/talk-to-us">
           <Button size="lg" variant="accent">
             Talk to us
           </Button>
         </Link>
-      </div>
+      }
     </div>
   );
 }
